d31a052811bcb2760afe31c02601460891223dbb
Brad Bailey <bbailey921@gmail.com>
2019-08-12 15:20:51 -0400

    add routes for podcasts

--- PodcastApp/controllers/podcast.js
+++ PodcastApp/controllers/podcast.js
@@ -3,6 +3,35 @@ const express = require('express')
 const podcastApi = require('../models/podcast.js')
 const podcastRouter = express.Router()
 
+podcastRouter.get('/', async (req, res) => {
+	try {
+		const podcasts = await podcastApi.getPodcastByPlaylist(
+			req.params.playlistId
+		)
+		res.json(podcasts)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+podcastRouter.get('/:podcastId', async (req, res) => {
+	try {
+		const singlePodcast = await podcastApi.getPodcast(req.params.podcastId)
+		res.json(podcastId)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
+podcastRouter.post('/', async (req, res) => {
+	try {
+		const newPodcast = await podcastApi.newPodcast(req.body)
+		res.json(newPodcast)
+	} catch (err) {
+		console.log(err)
+	}
+})
+
 module.exports = {
 	podcastRouter
 }
