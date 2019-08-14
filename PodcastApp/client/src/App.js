7296e2bfd93df9210104c9c6afa5b35c527be273
Brad Bailey <bbailey921@gmail.com>
2019-08-14 11:12:08 -0400

    create navbar component

--- PodcastApp/client/src/App.js
+++ PodcastApp/client/src/App.js
@@ -1,18 +1,20 @@
-import React from 'react';
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
 import HelloWorld from './components/HelloWorld.js'
-import './App.css';
+import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Switch>
-          <Route exact path="/" component={HelloWorld}/>
-        </Switch>
-      </Router>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Router>
+				<Navbar />
+				<Switch>
+					<Route exact path='/' component={HelloWorld} />
+				</Switch>
+			</Router>
+		</div>
+	)
 }
 
-export default App;
+export default App
